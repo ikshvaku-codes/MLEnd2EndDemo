06c36ca77d360caa5517701b0f8f516e4bab219b
ikshvaku <be.divanshu@gmail.com>
2023-01-08 21:00:20 +0530

    Initiate Transformation and Validation

---
+++ ccDefault/component/data_transformation.py
@@ -0,0 +1,19 @@
+import os, sys
+from ccDefault.exception import CCDefaultException
+
+
+
+"""
+        try:
+            pass
+        except Exception as e:
+                    raise CCDefaultException(e,sys) from e
+"""
+
+
+class DataTransformation:
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+                    raise CCDefaultException(e,sys) from e
