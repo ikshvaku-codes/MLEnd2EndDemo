f0c5a38aac6fb672538e38588cf75564dac8eea7
ikshvaku <be.divanshu@gmail.com>
2023-01-08 19:21:41 +0530

    Including log to catch block of main function

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from ccDefault.pipeline.pipeline import Pipeline
-
+from ccDefault.logger import logging
 def main():
-    pipeline = Pipeline()
-    pipeline.run_pipeline()
-    
+    try:
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+    except Exception as e:
+        logging.error(f"{e}")
+        
+        
 if __name__ == '__main__':
     main()
